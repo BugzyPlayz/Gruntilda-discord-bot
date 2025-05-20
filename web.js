420dc810f4ea3c4046652e05f253072da3c01b81
Bugzy <bugzyrecordz@gmail.com>
2025-05-20 16:12:23 -0400

    modified:   web.js

--- web.js
+++ web.js
@@ -21,3 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Listening on http://localhost:${PORT}`);
 });
+
+app.use((req, res) => {
+  res.status(404).send(`404 Not Found: ${req.originalUrl}`);
+});
