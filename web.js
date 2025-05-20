18fb392da834ebd8e7684a7417d81eaaac46a319
Bugzy <bugzyrecordz@gmail.com>
2025-05-20 15:50:55 -0400

    modified:   web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Home page working!');
+});
+
 app.get('/callback', (req, res) => {
   const code = req.query.code;
+  if (!code) {
+    return res.status(400).send("No code found in the query.");
+  }
   res.send(`Authorized! Your code: ${code}`);
   console.log("Callback received. Code:", code);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on http://localhost:3000/callback');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
